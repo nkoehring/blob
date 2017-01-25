500f9a8e03d0e7858f85686bbd1f085228ddc69b
Norman Köhring <norman.koehring@fromatob.com>
2017-01-25 17:18:54 +0100

    map IDs per payment method

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,50 @@ use std::io::prelude::*;
 use std::io;
 use std::fs::File;
 use std::path::Path;
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, BTreeMap};
 
 use regex::Regex;
 
 
-fn read_ids<P>(filename: P) -> BTreeSet<String>
-  where P: AsRef<Path>,
-  {
-    let mut set = BTreeSet::<String>::new();
-    let file = File::open(filename).expect("Cannot find IDs file!");
-    let reader = io::BufReader::new(file);
+fn read_ids<P>(filename: P) -> BTreeMap<String, String>
+where P: AsRef<Path>,
+{
+  let mut set = BTreeMap::<String, String>::new();
+  let file = File::open(filename).expect("Cannot find IDs file!");
+  let reader = io::BufReader::new(file);
 
-    for line in reader.lines() {
-      let l = line.unwrap();
-      set.insert(l.to_string());
-    }
-
-   set 
+  for line in reader.lines() {
+    let l = line.unwrap();
+    set.insert(l.to_string(), "".to_string());
   }
 
+ set 
+}
 
-fn check_log(mut ids: BTreeSet<String>) -> Counts {
+fn read_csv<P>(filename: P) -> BTreeMap<String, String>
+where P: AsRef<Path>,
+{
+  let mut map = BTreeMap::<String, String>::new();
+  let file = File::open(filename).expect("Cannot find CSV!");
+  let reader = io::BufReader::new(file);
+
+  for line in reader.lines() {
+    let l = line.unwrap();
+    let parts: Vec<&str> = l.split(",").collect();
+
+    if parts.len() < 3 {
+      println!("WARNING! Ignoring invalid CSV line: {:?}", l);
+      continue;
+    }
+
+    map.insert(parts[2].to_string(), parts[1].to_string());
+  }
+
+  map
+}
+
+
+fn check_log(mut ids: BTreeMap<String, String>) -> Counts {
     // capture groups:
     //   1: path (for example "payment/8cff8e51-5dab-4c72-b534-25b73466d2e3")
     //   2: id if path is hobbit
@@ -42,6 +64,14 @@ fn check_log(mut ids: BTreeSet<String>) -> Counts {
       hobbit_paid: 0,
       columbus_paid: 0,
       total_paid: ids.len(),
+      hobbit_credit_card: 0,
+      hobbit_paypal: 0,
+      hobbit_paypal_vault: 0,
+      hobbit_sofort: 0,
+      columbus_credit_card: 0,
+      columbus_paypal: 0,
+      columbus_paypal_vault: 0,
+      columbus_sofort: 0,
     };
 
     // save ip:id combination to ensure unique counts
@@ -67,7 +97,16 @@ fn check_log(mut ids: BTreeSet<String>) -> Counts {
 
             if set.insert(ip) {
               count.hobbit += 1;
-              if ids.remove(id) { count.hobbit_paid += 1; }
+              if let Some(value) = ids.remove(id) {
+                count.hobbit_paid += 1;
+                match value.as_ref() {
+                  "paypal" => count.hobbit_paypal += 1,
+                  "paypal_vault" => count.hobbit_paypal_vault += 1,
+                  "credit_card" => count.hobbit_credit_card += 1,
+                  "sofort" => count.hobbit_sofort += 1,
+                  _ => println!("Whoops? Hobbit?!"),
+                }
+              }
             }
 
           // capture group 3 only exists for columbus URLs
@@ -77,7 +116,16 @@ fn check_log(mut ids: BTreeSet<String>) -> Counts {
 
             if set.insert(ip) {
               count.columbus += 1;
-              if ids.remove(id) { count.columbus_paid += 1; }
+              if let Some(value) = ids.remove(id) {
+                count.columbus_paid += 1;
+                match value.as_ref() {
+                  "paypal" => count.columbus_paypal += 1,
+                  "paypal_vault" => count.columbus_paypal_vault += 1,
+                  "credit_card" => count.columbus_credit_card += 1,
+                  "sofort" => count.columbus_sofort += 1,
+                  _ => println!("Whoops? Columbus?!"),
+                }
+              }
             }
           }
         }
@@ -94,6 +142,14 @@ struct Counts {
   hobbit_paid: usize,
   columbus_paid: usize,
   total_paid: usize,
+  hobbit_credit_card: usize,
+  hobbit_paypal: usize,
+  hobbit_paypal_vault: usize,
+  hobbit_sofort: usize,
+  columbus_credit_card: usize,
+  columbus_paypal: usize,
+  columbus_paypal_vault: usize,
+  columbus_sofort: usize,
 }
 
 fn main() {
@@ -104,7 +160,11 @@ fn main() {
   }
 
   let ids_path = args[1].clone();
-  let ids = read_ids(ids_path);
+
+  let ids = match ids_path.ends_with(".csv") {
+    true => read_csv(ids_path),
+    false => read_ids(ids_path),
+  };
   let count = check_log(ids);
 
   /* TODO: conversion rates are wrong as long as not only unique visits are counted
@@ -112,10 +172,12 @@ fn main() {
   let columbus_con_perc = (count.columbus_paid as f64) * 100.0 / (count.columbus as f64);
   */
 
-  let hobbit_pay_perc = (count.hobbit_paid as f64) * 100.0 / (count.total_paid as f64);
-  let columbus_pay_perc = (count.columbus_paid as f64) * 100.0 / (count.total_paid as f64);
+  let hobbit_pay_perc = (count.hobbit_paid as f64) * 100.0 / ((count.hobbit_paid + count.columbus_paid) as f64);
+  let columbus_pay_perc = (count.columbus_paid as f64) * 100.0 / ((count.hobbit_paid + count.columbus_paid) as f64);
 
   println!("hobbit:   {:3} calls lead to {:3} payments, that's {:.2}% of payments", count.hobbit, count.hobbit_paid, hobbit_pay_perc);
   println!("columbus: {:3} calls lead to {:3} payments, that's {:.2}% of payments", count.columbus, count.columbus_paid, columbus_pay_perc);
   println!("{} of {} total payments", count.hobbit_paid + count.columbus_paid, count.total_paid);
+  println!("Hobbit   pp / cc / sofort: {:3} / {:3} / {:3}", count.hobbit_paypal + count.hobbit_paypal_vault, count.hobbit_credit_card, count.hobbit_sofort);
+  println!("Columbus pp / cc / sofort: {:3} / {:3} / {:3}", count.columbus_paypal + count.columbus_paypal_vault, count.columbus_credit_card, count.columbus_sofort);
 }
